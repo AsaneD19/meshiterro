b8bf2f5804a0ba49133c9149c289f02ea0e4a3e4
AsaneD19 <159635329+AsaneD19@users.noreply.github.com>
2024-03-15 14:50:16 +0000

    delete function implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
 
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: "homes#top"
+  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  devise_for :users
+  root  "homes#top"
   get 'homes/top'
+  get "homes/about" => "homes#about", as: "about"
 end
