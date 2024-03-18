04cf96d5b7418c67123d9368b19ec694f8c7c2b9
AsaneD19 <159635329+AsaneD19@users.noreply.github.com>
2024-03-18 04:33:34 +0000

    ~Chapter20

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,6 +3,7 @@ class PostImage < ApplicationRecord
   has_one_attached :image
   belongs_to :user
   has_many :post_comments, dependent: :destroy
+  has_many :favorites, dependent: :destroy
 
   def get_image
 
@@ -15,4 +16,7 @@ class PostImage < ApplicationRecord
 
   end
 
+  def favorited_by?(user)
+    favorites.exists?(user_id: user.id)
+  end
 end
