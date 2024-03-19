82cac45a495a85bc3d3bae274277ae0ca6b5fb47
AsaneD19 <159635329+AsaneD19@users.noreply.github.com>
2024-03-19 22:11:41 +0000

    ~Chapter24

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,13 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+const webpack = require('webpack')
+environment.plugins.prepend (
+  'provide',
+  new webpack.providePlugin({
+    $: 'jquery/src/jquery',
+    jquery: 'jwuery/src/jquery',
+    popper: 'popper.js'
+  })
+)
